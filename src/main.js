aa3d7767e68e4ed2979f0b12ac79fd311c7e2cea
wangxiaoyang1027 <wangxy_ly@aliyun.comm>
2019-12-15 00:14:19 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router/index.js'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  // 可以简写 router,
+  router:router,
 }).$mount('#app')
