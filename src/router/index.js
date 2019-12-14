ef102e4e7f400672b180ee4bcf947de0902b0223
wangxiaoyang1027 <wangxy_ly@aliyun.comm>
2019-12-15 00:11:30 +0800

    test

---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+// 配置所有的路由信息
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+
+import Home from '../components/home'
+import About from '../component/about'
+
+// 1. 通过Vue.use(插件) ， 来安装插件
+Vue.use(VueRouter)
+
+// 2. 创建路由对象
+const routes =[{
+    path:'./home',
+    component:Home
+},
+{
+    path:'./about',
+    component:About
+}]
+const router = new VueRouter({
+    // 配置路由和组件之间的应用关系
+    routes 
+
+    }
+)
+
+// 3. 将router 对象传入到 Vue
+export default router
